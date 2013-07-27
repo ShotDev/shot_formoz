61ea481f3be757558fc46e5b7d7b4d9107a2d084
Yang-Hsing Lin <yanghsing.lin@thekono.com>
2013-07-27 15:55:14 +0800

    add User::facebook_token_matched?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+  FB_GRAPH_URL = "https://graph.facebook.com"
   attr_accessible :facebook_id, :facebook_token
+
+  def self.facebook_token_matched? facebook_id, facebook_token
+    response = get("#{FB_GRAPH_URL}/me?access_token=#{facebook_token}")
+
+    user_data = JSON.parse(response.body)
+    user_data["id"] == facebook_id
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "User::facebook_token_matched?" do
+    let(:facebook_id) { "the-id" }
+    let(:facebook_token) { "the-token" }
+    let(:mock_success_response) { stub(:body => { :id => "the-id" }.to_json) }
+    let(:mock_failed_response) { stub(:body => { :error => "the-error" }.to_json) }
+
+    it "sends request to facebook with token" do
+      User.should_receive(:get).
+        with("#{User::FB_GRAPH_URL}/me?access_token=#{facebook_token}").
+        and_return(mock_success_response)
+
+      User.facebook_token_matched? facebook_id, facebook_token
+    end
+
+    it "returns true if response.id == facebook_id" do
+      User.stub(:get => mock_success_response) 
+
+      matched = User.facebook_token_matched? facebook_id, facebook_token
+
+      matched.should == true
+    end
+
+    it "returns false if facebook error" do
+      User.stub(:get => mock_failed_response)
+
+      matched = User.facebook_token_matched? facebook_id, facebook_token
+
+      matched.should == false
+    end
+  end
 end
